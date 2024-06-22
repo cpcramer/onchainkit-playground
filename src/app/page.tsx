2cd1ad50a4399eafbd7df42d6d7e9f0c1aacdf9f
Paul Cramer <paulccramer@gmail.com>
2024-06-22 19:25:15 -0400

    Background gradient finalized :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hey
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div id="home-text" className="z-10 items-center justify-between text-3xl lg:flex">
+        Swap Meme Coins on Base!
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,18 @@ const config: Config = {
       screens: {
         md: '848px',
       },
+      colors: {
+        'blue': '#1fb6ff',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        display: 'DM Sans, sans-serif',
+      },
     },
   },
   plugins: [],
