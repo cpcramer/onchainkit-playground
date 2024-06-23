a0c97c464d89821da6e40222a3b18d025719929a
Paul Cramer <paulccramer@gmail.com>
2024-06-22 22:39:15 -0400

    Saving progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles.css";
+import "@coinbase/onchainkit/tailwind.css";
+import "@coinbase/onchainkit/styles.css";
+import OnchainProviders from "../OnchainProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Meme Swap",
-  description: "Swap meme coins on Base",
+  title: "Onchain Playground",
+  description: "Build onchain applications with ease",
 };
 
 export default function RootLayout({
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* TODO: how does this wrapping work */}
+        <OnchainProviders>{children}</OnchainProviders>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import Title from "../pages/Title";
-import Swap from "../pages/Swap";
+
+import Title from "../components/Title"
+import Swap from "../components/Swap"
+import AccountConnect from "@/components/AccountConnect";
 
 export default function Home() {
   return (
-    <main id="main-text" className="max-w-screen-xl mx-auto relative border border-white md:px-0 md:mb-[64px] min-h-screen">
-      <header className="w-full py-4 bg-gray-800 text-white flex justify-between items-center px-6">
-        <div className="text-xl font-bold">
-          Base Onchain
-        </div>
-        <a href="#" className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white">
-          Connect Wallet
+    <main
+      id="main-text"
+      className="max-w-screen-xl mx-auto relative border border-white md:px-0 md:mb-[64px] min-h-screen"
+    >
+      <header className="w-full py-4 text-white flex justify-between items-center px-6">
+        <div className="text-xl font-bold">Onchain Playground</div>
+        <a
+          href="#"
+          className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white"
+        >
+          ConnectAccount
         </a>
+        {/* <div>
+          < AccountConnect />
+        </div> */}
       </header>
       <div className="flex justify-center items-center p-6 border border-white min-h-[80vh]">
         <div className="flex flex-col items-center justify-center">
